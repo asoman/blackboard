f0766a4a20680f1a12633aea17bbb0f7cce3bb45
Nikolay <asoman97@gmail.com>
2015-01-11 23:45:33 +0300

    auth_inprogress
    Начал работать над аутентицикацией

---
+++ www/js/auth.js
@@ -0,0 +1,21 @@
+
+//     
+//dialog = $( "#dialog-form" ).dialog({
+//autoOpen: false,
+//height: 300,
+//width: 350,
+//modal: true,
+//buttons: {
+//"Create an account": addUser,
+//Cancel: function() {
+//dialog.dialog( "close" );
+//}
+//},
+//close: function() {
+//form[ 0 ].reset();
+//allFields.removeClass( "ui-state-error" );
+//}
+//});
+ 
+
+
